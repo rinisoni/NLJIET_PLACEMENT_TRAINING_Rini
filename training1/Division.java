5dae0d4d312cdccd2c3cb9ef8d38fc11b436f3cd
Rini Soni <123010597+rinisoni@users.noreply.github.com>
2024-07-17 21:54:59 +0530

    Division.java

--- training1/Division.java
+++ training1/Division.java
@@ -3,7 +3,7 @@ package training1;
 public class Division {
 	public static void main(String[] args) {
 		int num1=2;
-	    int num2=3;
+	        int num2=3;
 		float result = (float) num1/num2;
 		System.out.print(result);
 	}
